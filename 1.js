8a1d63ced73da31c46603738f7ad37457e8d27db
fov42550564 <42550564@qq.com>
2018-04-04 17:29:05 +0800

    update

--- 1.js
+++ 1.js
@@ -27,7 +27,7 @@ function my_block(state, start, end, silent) {
     let pos = state.bMarks[start] + state.tShift[start];
     let max = state.eMarks[start];
     const text = state.src;
-
+    console.log('--------', text);
     if (pos + 5 > max) {
         return false;
     }
@@ -37,8 +37,8 @@ function my_block(state, start, end, silent) {
     pos += 5;
 
     let foundTail = false;
-    let firstLine = text.subtring(pos, max);
-    let lastLine = '';
+    let firstLine = text.slice(pos, max);
+    let lastLine;
     if(firstLine.trim().slice(-5) === 'fang:'){
         firstLine = firstLine.trim().slice(0, -5);
         foundTail = true;
@@ -57,22 +57,33 @@ function my_block(state, start, end, silent) {
     }
     state.line = line;
 
-    const token = state.push('fang_block', 'fang', 0);
-    token.block = true;
-    token.content = firstLine + '\n';
-    token.content += state.getLines(start + 1, line, state.tShift[start], true);
-    token.content += lastLine;
+    if (foundTail) {
+        const token = state.push('fang_block', 'fang', 0);
+        token.block = true;
+        token.content = firstLine;
+        if (lastLine) {
+            token.content += '\n' + state.getLines(start + 1, line, state.tShift[start], true);
+            token.content += lastLine.trim();
+        }
 
-    token.map = [ start, state.line ];
+        token.map = [ start, state.line ];
 
-    console.log(token);
+        console.log(token);
+    }
+
 
     return true;
+}
+
+function blockRenderer(tokens, idx) {
+    const { content, tag } = tokens[idx];
+    return `<${tag}>${content}</${tag}>`;
 }
 
 function my_plugin(md, options) {
     // md.inline.ruler.before('text', 'my_inline', my_inline);
     md.block.ruler.push('my_block', my_block);
+    md.renderer.rules.fang_block = blockRenderer;
 }
 
 md.use(my_plugin);
