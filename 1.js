6331b5384c267515bfd71691b8969ccfed279e67
fov42550564 <42550564@qq.com>
2018-04-04 17:17:24 +0800

    完成block解析

--- 1.js
+++ 1.js
@@ -1,11 +1,13 @@
 var md = require('./markdown-it')();
 var _ = require('lodash');
 
+
+
 function my_inline(state, silent) {
     console.log(_.omit(state, 'md', 'Token', 'scanDelims', 'push', 'pushPending'));
     return false;
 }
-function my_block(state, start, end, silent) {
+function my_block1(state, start, end, silent) {
     // console.log('------', start, end);
     // console.log(_.omit(state, 'md', 'push', 'Token', 'isEmpty', 'skipEmptyLines', 'skipSpaces', 'skipSpacesBack', 'skipChars', 'skipCharsBack', 'getLines'));
     const text = state.src;
@@ -20,6 +22,54 @@ function my_block(state, start, end, silent) {
     return false;
 }
 
+// 找出以 :fang 开头， 以 fang: 结尾的，将 fang 替换为 yun
+function my_block(state, start, end, silent) {
+    let pos = state.bMarks[start] + state.tShift[start];
+    let max = state.eMarks[start];
+    const text = state.src;
+
+    if (pos + 5 > max) {
+        return false;
+    }
+    if(text.slice(pos, pos + 5) !== ':fang') {
+        return false;
+    }
+    pos += 5;
+
+    let foundTail = false;
+    let firstLine = text.subtring(pos, max);
+    let lastLine = '';
+    if(firstLine.trim().slice(-5) === 'fang:'){
+        firstLine = firstLine.trim().slice(0, -5);
+        foundTail = true;
+    }
+
+    let line = start + 1;
+    while (!foundTail && line < end) {
+        pos = state.bMarks[line] + state.tShift[line];
+        max = state.eMarks[line];
+        const lineText = text.slice(pos, max).trim();
+        if(lineText.slice(-5) === 'fang:') {
+            lastLine = lineText.slice(0, -5);
+            foundTail = true;
+        }
+        line++;
+    }
+    state.line = line;
+
+    const token = state.push('fang_block', 'fang', 0);
+    token.block = true;
+    token.content = firstLine + '\n';
+    token.content += state.getLines(start + 1, line, state.tShift[start], true);
+    token.content += lastLine;
+
+    token.map = [ start, state.line ];
+
+    console.log(token);
+
+    return true;
+}
+
 function my_plugin(md, options) {
     // md.inline.ruler.before('text', 'my_inline', my_inline);
     md.block.ruler.push('my_block', my_block);
@@ -27,6 +77,7 @@ function my_plugin(md, options) {
 
 md.use(my_plugin);
 
+var str = `:fang 123 fang:`;
 
-var result = md.render('1\n\n2');
+var result = md.render(str);
 console.log('\n', result);
