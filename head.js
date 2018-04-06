4504f0471a06daa6ed356713c158e91327fe75d2
fov42550564 <42550564@qq.com>
2018-04-06 21:50:24 +0800

    update

--- head.js
+++ head.js
@@ -2,21 +2,20 @@
 
 const ANCHOR_SVG = '<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>';
 
-function getSerial(serialPool, level) {
-    let list = [];
-    for (const l in serialPool) {
-        list.push(serialPool[l]);
-        if (l == level) {
-            break;
-        }
-    }
-    return list;
-}
-
 module.exports = function(md, options) {
     let gstate;
     let hasToc = false;
 
+    function getSerial(serialPool, level) {
+        let list = [];
+        for (const l in serialPool) {
+            list.push(serialPool[l]);
+            if (l == level) {
+                break;
+            }
+        }
+        return list;
+    }
     function toc_block(state, startLine, endLine, silent) {
         let pos = state.bMarks[startLine] + state.tShift[startLine];
         let max = state.eMarks[startLine];
@@ -113,7 +112,7 @@ module.exports = function(md, options) {
                     indent--;
                 }
             }
-            res.push(`<li><a class="anchor" aria-hidden="true" href="#${head_id}" id="${toc_id}">${options.autoNumber ? `<span style="margin-right: 6px;">${serial}</span>` : ''}${name}</a></li>`);
+            res.push(`<li style="list-style:none;"><a class="anchor" aria-hidden="true" href="#${head_id}" id="${toc_id}">${options.autoNumber ? `<span style="margin-right: 12px;">${serial}</span>` : ''}${name}</a></li>`);
             return res.join('');
         });
 
